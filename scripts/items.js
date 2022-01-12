64334e1fea18208562a1229cee5213539c553f92
romanunr02 <romanunr02@gmail.com>
2022-01-12 23:47:15 +0200

    c

--- scripts/items.js
+++ scripts/items.js
@@ -1,32 +1,16 @@
-// const ceramic = extendContent(Item, "ceramic", {});
-// const clay = extendContent(Item, "clay", {});
-// const crystal = extendContent(Item, "crystal", {});
-// const hematite = extendContent(Item, "hematite", {});
-// const iron = extendContent(Item, "iron", {});
-// const nitrogen = extendContent(Item, "nitrogen", {});
-// const plutonium = extendContent(Item, "plutonium", {});
-// const rawCrystal = extendContent(Item, "raw-crystal", {});
-// const salt = extendContent(Item, "salt", {});
-// const sphalerite = extendContent(Item, "sphalerite", {});
-// const steel = extendContent(Item, "steel", {});
-// const uranium = extendContent(Item, "uranium", {});
-// const zinc = extendContent(Item, "zinc", {});
-
-
-
-const ceramic = new Item("ceramic")
-const clay =new Item("clay")
-const crystal = new Item("crystal")
-const hematite = new Item("hematite")
-const iron =new Item("iron")
-const nitrogen =new Item("nitrogen")
-const plutonium =new Item("plutonium")
-const rawCrystal =new Item("raw-crystal")
-const salt =new Item("salt")
-const sphalerite =new Item("sphalerite")
-const steel =new Item("steel")
-const uranium =new Item("uranium")
-const zinc = new Item("zinc")
+const ceramic = extendContent(Item, "ceramic", {});
+const clay = extendContent(Item, "clay", {});
+const crystal = extendContent(Item, "crystal", {});
+const hematite = extendContent(Item, "hematite", {});
+const iron = extendContent(Item, "iron", {});
+const nitrogen = extendContent(Item, "nitrogen", {});
+const plutonium = extendContent(Item, "plutonium", {});
+const rawCrystal = extendContent(Item, "raw-crystal", {});
+const salt = extendContent(Item, "salt", {});
+const sphalerite = extendContent(Item, "sphalerite", {});
+const steel = extendContent(Item, "steel", {});
+const uranium = extendContent(Item, "uranium", {});
+const zinc = extendContent(Item, "zinc", {});
 
 module.exports = {
     ceramic: ceramic,
