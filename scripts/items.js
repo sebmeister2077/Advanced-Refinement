67aa0a027b9740ed1fdedcdb0a581a51812812a2
romanunr02 <romanunr02@gmail.com>
2022-01-12 23:32:08 +0200

    trying to fix bug of now showing items in editor

--- scripts/items.js
+++ scripts/items.js
@@ -2,28 +2,47 @@ const ceramic = extendContent(Item, "ceramic", {});
 const clay = extendContent(Item, "clay", {});
 const crystal = extendContent(Item, "crystal", {});
 const hematite = extendContent(Item, "hematite", {});
-const iron = extendContent(Item, "iron", {});
-const nitrogen = extendContent(Item, "nitrogen", {});
-const plutonium = extendContent(Item, "plutonium", {});
-const rawCrystal = extendContent(Item, "raw-crystal", {});
-const salt = extendContent(Item, "salt", {});
-const sphalerite = extendContent(Item, "sphalerite", {});
-const steel = extendContent(Item, "steel", {});
-const uranium = extendContent(Item, "uranium", {});
-const zinc = extendContent(Item, "zinc", {});
+// const iron = extendContent(Item, "iron", {});
+// const nitrogen = extendContent(Item, "nitrogen", {});
+// const plutonium = extendContent(Item, "plutonium", {});
+// const rawCrystal = extendContent(Item, "raw-crystal", {});
+// const salt = extendContent(Item, "salt", {});
+// const sphalerite = extendContent(Item, "sphalerite", {});
+// const steel = extendContent(Item, "steel", {});
+// const uranium = extendContent(Item, "uranium", {});
+// const zinc = extendContent(Item, "zinc", {});
 
-module.exports = {
-    ceramic: ceramic,
-    clay: clay,
-    crystal: crystal,
-    hematite: hematite,
-    iron: iron,
-    nitrogen: nitrogen,
-    plutonium: plutonium,
-    rawCrystal: rawCrystal,
-    salt: salt,
-    sphalerite: sphalerite,
-    steel: steel,
-    uranium: uranium,
-    zinc: zinc
-}
+// module.exports = {
+//     ceramic: ceramic,
+//     clay: clay,
+//     crystal: crystal,
+//     hematite: hematite,
+//     iron: iron,
+//     nitrogen: nitrogen,
+//     plutonium: plutonium,
+//     rawCrystal: rawCrystal,
+//     salt: salt,
+//     sphalerite: sphalerite,
+//     steel: steel,
+//     uranium: uranium,
+//     zinc: zinc
+// }
+
+new Item("ceramic")
+new Item("clay")
+new Item("crystal")
+new Item("hematite")
+new Item("iron")
+new Item("nitrogen")
+new Item("plutonium")
+new Item("raw-crystal")
+new Item("salt")
+new Item("sphalerite")
+new Item("steel")
+new Item("uranium")
+new Item("zinc")
+new Liquid("carbonic-acid")
+new Liquid("impure-nitric-acid")
+new Liquid("liquid-nitrogen")
+new Liquid("mineral-water")
+new Liquid("nitric-acid")
