657b0c7db41786e7b57aa42f83ca5ddec49c6d1e
romanunr02 <romanunr02@gmail.com>
2022-02-03 21:59:29 +0200

    commented script

--- scripts/liquids.js
+++ scripts/liquids.js
@@ -5,14 +5,14 @@ new Liquid("mineral-water")
 new Liquid("nitric-acid")
 
 
-const carbonicAcid = extendContent(Liquid, "carbonic-acid", {});
-const impureNitricAcid = extendContent(Liquid, "impure-nitric-acid", {});
-const liquidNitrogen = extendContent(Liquid, "liquid-nitrogen", {});
-const nitricAcid = extendContent(Liquid, "nitric-acid", {});
+// const carbonicAcid = extendContent(Liquid, "carbonic-acid", {});
+// const impureNitricAcid = extendContent(Liquid, "impure-nitric-acid", {});
+// const liquidNitrogen = extendContent(Liquid, "liquid-nitrogen", {});
+// const nitricAcid = extendContent(Liquid, "nitric-acid", {});
 
-module.exports = {
-    carbonicAcid: carbonicAcid,
-    impureNitricAcid: impureNitricAcid,
-    liquidNitrogen: liquidNitrogen,
-    nitricAcid: nitricAcid
-}
+// module.exports = {
+//     carbonicAcid: carbonicAcid,
+//     impureNitricAcid: impureNitricAcid,
+//     liquidNitrogen: liquidNitrogen,
+//     nitricAcid: nitricAcid
+// }
