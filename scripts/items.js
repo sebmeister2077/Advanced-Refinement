0987319cd1af3ae0be141287bd5ee37bb8a482fa
romanunr02 <romanunr02@gmail.com>
2022-02-06 00:08:39 +0200

    removed unused code

--- scripts/items.js
+++ scripts/items.js
@@ -1,32 +1,3 @@
-// const ceramic = extendContent(Item, "ceramic", {});
-// const clay = extendContent(Item, "clay", {});
-// const crystal = extendContent(Item, "crystal", {});
-// const hematite = extendContent(Item, "hematite", {});
-// const iron = extendContent(Item, "iron", {});
-// const nitrogen = extendContent(Item, "nitrogen", {});
-// const plutonium = extendContent(Item, "plutonium", {});
-// const rawCrystal = extendContent(Item, "raw-crystal", {});
-// const salt = extendContent(Item, "salt", {});
-// const sphalerite = extendContent(Item, "sphalerite", {});
-// const steel = extendContent(Item, "steel", {});
-// const uranium = extendContent(Item, "uranium", {});
-// const zinc = extendContent(Item, "zinc", {});
-
-// module.exports = {
-//     ceramic: ceramic,
-//     clay: clay,
-//     crystal: crystal,
-//     hematite: hematite,
-//     iron: iron,
-//     nitrogen: nitrogen,
-//     plutonium: plutonium,
-//     rawCrystal: rawCrystal,
-//     salt: salt,
-//     sphalerite: sphalerite,
-//     steel: steel,
-//     uranium: uranium,
-//     zinc: zinc
-// }
 
 new OreBlock("clay", new Item("clay"))
 new OreBlock("raw-crystal", new Item("raw-crystal"))
