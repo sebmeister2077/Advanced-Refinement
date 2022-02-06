35d8ada538da76063ba5e54275297fca87dccb34
romanunr02 <romanunr02@gmail.com>
2022-02-06 15:25:28 +0200

    adding vars to anonymous instances

--- scripts/liquids.js
+++ scripts/liquids.js
@@ -1,10 +1,10 @@
-new Liquid("carbonic-acid")
+const carbonicAcid = new Liquid("carbonic-acid")
 new Liquid("impure-nitric-acid")
 new Liquid("liquid-nitrogen")
 new Liquid("mineral-water")
 new Liquid("nitric-acid")
 
 
-const carbonicAcid = new Floor("carbonic-acid")
-carbonicAcid.isLiquid = true;
-carbonicAcid.liquidDrop = Liquids.carbonic-acid;
+const carbonicAcidFloor = new Floor("carbonic-acid")
+carbonicAcidFloor.isLiquid = true;
+carbonicAcidFloor.liquidDrop = Liquids.carbonicAcid;
