86c2aca62e97f8a16c94e47cb17cc63bb9705209
romanunr02 <romanunr02@gmail.com>
2022-02-06 15:33:42 +0200

    added more params

--- scripts/liquids.js
+++ scripts/liquids.js
@@ -1,10 +1,17 @@
 const carbonicAcid = new Liquid("carbonic-acid")
+const mineralWater = new Liquid("mineral-water")
 new Liquid("impure-nitric-acid")
 new Liquid("liquid-nitrogen")
-new Liquid("mineral-water")
 new Liquid("nitric-acid")
 
 
-const carbonicAcidFloor = new Floor("carbonic-acid")
-carbonicAcidFloor.isLiquid = true;
-carbonicAcidFloor.liquidDrop = carbonicAcid;
+const carbonicAcidFloor = new Floor("floor-carbonic-acid")
+carbonicAcidFloor = {
+    ...carbonicAcidFloor,
+    isLiquid = true,
+    liquidDrop = carbonicAcid,
+    drownTime = 100,
+    status = StatusEffects.melting,
+    statusDuration = 200,
+    cacheLayer = cacheLayers.slag,
+}
