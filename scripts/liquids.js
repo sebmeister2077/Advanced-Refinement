ff21ffbae11fd0cca6eae52173d9a0ecfac5ec0d
romanunr02 <romanunr02@gmail.com>
2022-02-06 15:38:50 +0200

    renamed item

--- scripts/liquids.js
+++ scripts/liquids.js
@@ -5,10 +5,20 @@ new Liquid("liquid-nitrogen")
 new Liquid("nitric-acid")
 
 
-const carbonicAcidFloor = new Floor("floor-carbonic-acid")
-carbonicAcidFloor.isLiquid = true;
-carbonicAcidFloor.liquidDrop = carbonicAcid;
-carbonicAcidFloor.drownTime = 100;
-carbonicAcidFloor.status = StatusEffects.melting;
-carbonicAcidFloor.statusDuration = 200;
-carbonicAcidFloor.cacheLayer = cacheLayers.slag;
+const carbonicAcidFloor = new Floor("carbonic-acid")
+// carbonicAcidFloor.isLiquid = true;
+// carbonicAcidFloor.liquidDrop = carbonicAcid;
+// carbonicAcidFloor.drownTime = 100;
+// carbonicAcidFloor.status = StatusEffects.melting;
+// carbonicAcidFloor.statusDuration = 200;
+// carbonicAcidFloor.cacheLayer = cacheLayers.slag;
+
+carbonicAcidFloor = {
+    ...carbonicAcidFloor,
+    isLiquid: true,
+    liquidDrop: carbonicAcid,
+    drownTime: 100,
+    status: StatusEffects.melting,
+    statusDuration: 200,
+    cacheLayer: cacheLayers.slag,
+}
