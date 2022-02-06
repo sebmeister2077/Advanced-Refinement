8a619859c6cc491ae7228717e8dfb8f9d556fc4e
romanunr02 <romanunr02@gmail.com>
2022-02-06 12:24:10 +0200

    fixed typo

--- scripts/items.js
+++ scripts/items.js
@@ -2,4 +2,4 @@
 new OreBlock("clay", new Item("clay"))
 new OreBlock("raw-crystal", new Item("raw-crystal"))
 new OreBlock("hematite",new Item("hematite"))
-new OreBlock("spalerite",new Item("spalerite"))
+new OreBlock("spalerite",new Item("sphalerite"))
