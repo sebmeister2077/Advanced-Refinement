253fb1fc64a67f1da4415b7f814987f021059d12
romanunr02 <romanunr02@gmail.com>
2022-02-06 12:21:37 +0200

    automatically enable console

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-// require('blocks/siliconcompressor')
-// require('blocks/scrapcondensator')
 
-//init
 require('items');
 require('liquids');
+
+
+Vars.enableConsole = true;// so i wont have to use the dev mode
