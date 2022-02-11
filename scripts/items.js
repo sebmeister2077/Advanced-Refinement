e22d3fb337baea4688c5a45632d94e901dfb7db5
romanunr02 <romanunr02@gmail.com>
2022-02-11 20:12:02 +0200

    added snow ore

--- scripts/items.js
+++ scripts/items.js
@@ -2,4 +2,5 @@
 new OreBlock("clay", new Item("clay"))
 new OreBlock("raw-crystal", new Item("raw-crystal"))
 new OreBlock("hematite",new Item("hematite"))
-new OreBlock("spalerite",new Item("sphalerite"))
+new OreBlock("spalerite", new Item("sphalerite"))
+new OreBlock("snow", new Item("snow"));
