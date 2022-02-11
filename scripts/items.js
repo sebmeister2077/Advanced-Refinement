dd6e419043e0d625b350d6554b2a7fd1a50fb6fc
romanunr02 <romanunr02@gmail.com>
2022-02-11 20:12:25 +0200

    added ice oreblock

--- scripts/items.js
+++ scripts/items.js
@@ -4,3 +4,4 @@ new OreBlock("raw-crystal", new Item("raw-crystal"))
 new OreBlock("hematite",new Item("hematite"))
 new OreBlock("spalerite", new Item("sphalerite"))
 new OreBlock("snow", new Item("snow"));
+new OreBlock("ice", new Item("ice"));
