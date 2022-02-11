510a59bf2084b67c0d8b285e853856cb0dde3001
romanunr02 <romanunr02@gmail.com>
2022-02-11 20:23:22 +0200

    trying to add a dialog

--- scripts/items.js
+++ scripts/items.js
@@ -3,5 +3,5 @@ new OreBlock("clay", new Item("clay"))
 new OreBlock("raw-crystal", new Item("raw-crystal"))
 new OreBlock("hematite",new Item("hematite"))
 new OreBlock("spalerite", new Item("sphalerite"))
-new OreBlock("snow", new Item("snow"));
+new OreBlock("snow", new Item("snow"));//todo i need to override this
 new OreBlock("ice", new Item("ice"));
--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,11 @@ require('liquids');
 
 
 Vars.enableConsole = true;// so i wont have to use the dev mode
+
+const myDialog = new BaseDialog("Dialog Title");
+// Add "go back" button
+myDialog.addCloseButton();
+// Add text to the main content
+myDialog.cont.add("Goodbye.");
+// Show dialog
+myDialog.show();
