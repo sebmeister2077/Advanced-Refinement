1dbc66bf23021654ade482766fb1532b5277c0f5
romanunr02 <romanunr02@gmail.com>
2022-02-12 22:55:38 +0200

    added ice item as drop

--- scripts/items.js
+++ scripts/items.js
@@ -4,7 +4,8 @@ new OreBlock("raw-crystal", new Item("raw-crystal"))
 new OreBlock("hematite",new Item("hematite"))
 new OreBlock("spalerite", new Item("sphalerite"))
 
-new OreBlock("ice", new Item("ice"));//todo i need to override this
-
 const snowBlock = Vars.content.block("snow")
 snowBlock.itemDrop = new Item("snow");
+
+const iceBlock = Vars.content.block("ice");
+iceBlock.itemDrop = new Item("ice")
