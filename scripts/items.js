df5e3dad2939d1764359625fffae124c7fdb1320
romanunr02 <romanunr02@gmail.com>
2022-02-12 22:53:53 +0200

    made snow floor drop snow item

--- scripts/items.js
+++ scripts/items.js
@@ -3,5 +3,8 @@ new OreBlock("clay", new Item("clay"))
 new OreBlock("raw-crystal", new Item("raw-crystal"))
 new OreBlock("hematite",new Item("hematite"))
 new OreBlock("spalerite", new Item("sphalerite"))
-new OreBlock("snow", new Item("snow"));//todo i need to override this
-new OreBlock("ice", new Item("ice"));
+
+new OreBlock("ice", new Item("ice"));//todo i need to override this
+
+const snowBlock = Vars.content.block("snow")
+snowBlock.itemDrop = new Item("snow");
