86eb9c844e958cc56f04d9237016b1d58a04e406
romanunr02 <romanunr02@gmail.com>
2022-02-12 23:09:32 +0200

    refactored items script

--- scripts/items.js
+++ scripts/items.js
@@ -1,12 +1,25 @@
 
-new OreBlock("clay", new Item("clay"))
-new OreBlock("raw-crystal", new Item("raw-crystal"))
-new OreBlock("hematite",new Item("hematite"))
-new OreBlock("spalerite", new Item("sphalerite"))
-
-const snowBlock = Vars.content.block("snow")
-snowBlock.itemDrop = new Item("snow");
-
+//items
+const clayItem = new Item("clay")
+const rawCrystalItem = new Item("raw-crystal");
+const hematiteItem = new Item("hematite")
+const sphaleriteItem = new Item("sphalerite")
+const snowItem = new Item("snow");
 const iceItem = new Item("ice");
+
+
+//oreblocks
+const oreRawCrystal = new OreBlock("raw-crystal")
+const oreHematite = new OreBlock("hematite")
+const oreSphalerite = new OreBlock("spalerite")
+
+//floors
+
+const clayFloor = new Floor("clay")
+clayFloor.needsSurface = false;
+
+//overrides
+Vars.content.block("snow").itemDrop = snowItem
 Vars.content.block("ice").itemDrop = iceItem;
 Vars.content.block("ice-snow").itemDrop = iceItem;
+Vars.content.block("molten-slag").solidifies = true; // slag liquid can be made solid
