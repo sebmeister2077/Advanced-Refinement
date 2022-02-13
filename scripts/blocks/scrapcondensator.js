d531e5d70afcd2d7c0fc4d7a1adfecc5fc3634b7
romanunr02 <romanunr02@gmail.com>
2022-02-13 15:50:40 +0200

    commented more code

--- scripts/blocks/scrapcondensator.js
+++ scripts/blocks/scrapcondensator.js
@@ -4,5 +4,5 @@ scrapCondensator.hasLiquids = true;
 const scrapItem = Vars.content.item("scrap")
 if (!scrapItem)
     throw new Error("scrap item is null")
-scrapCondensator.itemDrop = scrapItem
+//scrapCondensator.itemDrop = scrapItem
 //scrapCondensator.outputsItems = () => true; // overrides the hardcoded value
