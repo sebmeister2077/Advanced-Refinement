85d1bc239c6d64a550f3769f207bcf6b52db41c0
romanunr02 <romanunr02@gmail.com>
2022-02-13 16:08:50 +0200

    removed unnecesary sprites for carbonic acid floor

--- scripts/liquids.js
+++ scripts/liquids.js
@@ -6,11 +6,14 @@ const liquidNitrogen = new Liquid("liquid-nitrogen")
 const nitricAcid = new Liquid("nitric-acid")
 
 //floors
-const carbonicAcidFloor = new Floor("carbonic-acid")
+const carbonicAcidFloor = new Floor("carbonic-acid-floor")
 carbonicAcidFloor.isLiquid = true;
 carbonicAcidFloor.liquidDrop = carbonicAcid;
 carbonicAcidFloor.damageTaken = 1;
 carbonicAcidFloor.speedMultiplier = 0.7;
 carbonicAcidFloor.drownTime = 360;
+carbonicAcidFloor.variants = 1;
+carbonicAcidFloor.emitLight = true;
+carbonicAcidFloor.lightRadius = 10;
 //carbonicAcidFloor.statusEffect = StatusEffects.corroded;
 carbonicAcidFloor.cacheLayer = CacheLayer.tar;
