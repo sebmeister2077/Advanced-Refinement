62570c58ee539a237f5c70bce59acd2d5987a2f8
romanunr02 <romanunr02@gmail.com>
2022-02-13 14:05:45 +0200

    made scripts more clean

--- scripts/liquids.js
+++ scripts/liquids.js
@@ -1,12 +1,16 @@
+//liquids
 const carbonicAcid = new Liquid("carbonic-acid")
 const mineralWater = new Liquid("mineral-water")
-new Liquid("impure-nitric-acid")
-new Liquid("liquid-nitrogen")
-new Liquid("nitric-acid")
+const impureNitricAcid = new Liquid("impure-nitric-acid")
+const liquidNitrogen = new Liquid("liquid-nitrogen")
+const nitricAcid = new Liquid("nitric-acid")
 
-
+//floors
 const carbonicAcidFloor = new Floor("carbonic-acid")
 carbonicAcidFloor.isLiquid = true;
 carbonicAcidFloor.liquidDrop = carbonicAcid;
 carbonicAcidFloor.damageTaken = 1;
+carbonicAcidFloor.speedMultiplier = 0.7;
+carbonicAcidFloor.drownSpeed = 360;
+carbonicAcidFloor.statusEffect = StatusEffects.corroded;
 carbonicAcidFloor.cacheLayer = CacheLayer.tar;
